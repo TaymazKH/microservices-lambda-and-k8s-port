5e3967c389366bbb33558623ed24654dfd02cb9f
Taymaz <TaymazKH.U@gmail.com>
2024-08-11 22:19:43 +0330

    initial experiment with http+proto

--- client/client.go
+++ client/client.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+    "bytes"
     "context"
     "flag"
+    "io"
     "log"
+    "net/http"
     "time"
 
-    "google.golang.org/grpc"
-    "google.golang.org/grpc/credentials/insecure"
+    "google.golang.org/protobuf/proto"
     pb "main/hello"
 )
 
@@ -18,20 +20,48 @@ var (
 
 func main() {
     flag.Parse()
-    // Set up a connection to the server.
-    conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-    if err != nil {
-        log.Fatalf("did not connect: %v", err)
-    }
-    defer conn.Close()
-    c := pb.NewGreeterClient(conn)
 
-    // Contact the server and print out its response.
-    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-    defer cancel()
-    r, err := c.SayHello(ctx, &pb.HelloRequest{Name: *name})
+    // Create the HelloRequest object
+    req := &pb.HelloRequest{Name: *name}
+
+    // Encode the HelloRequest object to binary (Protocol Buffers format)
+    binReq, err := proto.Marshal(req)
     if err != nil {
-        log.Fatalf("could not greet: %v", err)
+        log.Fatalf("Failed to encode request: %v", err)
     }
-    log.Printf("Greeting: %s", r.GetText())
+
+    // Create the HTTP request
+    httpReq, err := http.NewRequestWithContext(context.Background(), "POST", "http://"+*addr+"/sayhello", bytes.NewBuffer(binReq))
+    if err != nil {
+        log.Fatalf("Failed to create HTTP request: %v", err)
+    }
+    httpReq.Header.Set("Content-Type", "application/octet-stream")
+
+    // Send the HTTP request
+    client := &http.Client{Timeout: time.Second}
+    resp, err := client.Do(httpReq)
+    if err != nil {
+        log.Fatalf("Failed to send HTTP request: %v", err)
+    }
+    defer resp.Body.Close()
+
+    // Check if the response status is OK
+    if resp.StatusCode != http.StatusOK {
+        log.Fatalf("Received non-OK response: %s", resp.Status)
+    }
+
+    // Read the entire response body into a byte slice
+    respBody, err := io.ReadAll(resp.Body)
+    if err != nil {
+        log.Fatalf("Failed to read response body: %v", err)
+    }
+
+    // Decode the HelloResponse object from the binary response
+    var helloResp pb.HelloResponse
+    if err := proto.Unmarshal(respBody, &helloResp); err != nil {
+        log.Fatalf("Failed to decode response: %v", err)
+    }
+
+    // Print the greeting from the response
+    log.Printf("Greeting: %s", helloResp.GetText())
 }
--- server/server.go
+++ server/server.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-    "context"
     "flag"
     "fmt"
+    "io"
     "log"
-    "net"
+    "net/http"
 
-    "google.golang.org/grpc"
+    "google.golang.org/protobuf/proto"
     pb "main/hello"
 )
 
@@ -15,25 +15,62 @@ var (
     port = flag.Int("port", 50051, "The server port")
 )
 
-type server struct {
-    pb.UnimplementedGreeterServer
-}
+// sayHelloHandler handles the /sayhello endpoint
+func sayHelloHandler(w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodPost {
+        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+        return
+    }
 
-func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
-    log.Printf("Received: %v", in.GetName())
-    return &pb.HelloResponse{Text: "Hello " + in.GetName()}, nil
+    // Read the request body into a byte slice
+    reqBody, err := io.ReadAll(r.Body)
+    if err != nil {
+        http.Error(w, "Failed to read request body", http.StatusInternalServerError)
+        return
+    }
+    defer r.Body.Close()
+
+    // Unmarshal the request body into a HelloRequest message
+    var req pb.HelloRequest
+    if err := proto.Unmarshal(reqBody, &req); err != nil {
+        http.Error(w, "Failed to parse request", http.StatusBadRequest)
+        return
+    }
+
+    // Log the received name
+    log.Printf("Received: %v", req.GetName())
+
+    // Create a HelloResponse message
+    resp := &pb.HelloResponse{
+        Text: "Hello " + req.GetName(),
+    }
+
+    // Marshal the HelloResponse message into binary format
+    respBody, err := proto.Marshal(resp)
+    if err != nil {
+        http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+        return
+    }
+
+    // Set the content type and write the response body
+    w.Header().Set("Content-Type", "application/octet-stream")
+    _, err = w.Write(respBody)
+    if err != nil {
+        http.Error(w, "Failed to write response", http.StatusInternalServerError)
+    }
 }
 
 func main() {
     flag.Parse()
-    lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-    if err != nil {
-        log.Fatalf("failed to listen: %v", err)
-    }
-    s := grpc.NewServer()
-    pb.RegisterGreeterServer(s, &server{})
-    log.Printf("server listening at %v", lis.Addr())
-    if err := s.Serve(lis); err != nil {
-        log.Fatalf("failed to serve: %v", err)
+
+    // Set up the HTTP server and route
+    http.HandleFunc("/sayhello", sayHelloHandler)
+
+    addr := fmt.Sprintf(":%d", *port)
+    log.Printf("Server listening on %v", addr)
+
+    // Start the HTTP server
+    if err := http.ListenAndServe(addr, nil); err != nil {
+        log.Fatalf("Failed to start server: %v", err)
     }
 }
