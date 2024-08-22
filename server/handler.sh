910a66a11028a26adc5425a0894319aa38f0dd1b
Taymaz <TaymazKH.U@gmail.com>
2024-08-22 19:31:40 +0330

    modified handler to use jq

--- server/handler.sh
+++ server/handler.sh
@@ -1,7 +1,23 @@
 function handler () {
     EVENT_DATA=$1
-    echo "event data: $($EVENT_DATA)" >&2
+    echo "event data: $EVENT_DATA" >&2
 
-    RESPONSE="{\"statusCode\": 200, \"body\": \"Hello from Lambda!\"}"
+    BODY=$(echo $EVENT_DATA | /opt/jq/jq -r '.body')
+    HEADERS=$(echo $EVENT_DATA | /opt/jq/jq -r '.headers')
+    METHOD=$(echo $EVENT_DATA | /opt/jq/jq -r '.requestContext.http.method')
+    PATH=$(echo $EVENT_DATA | /opt/jq/jq -r '.requestContext.http.path')
+
+    HOST="localhost"
+    PORT=8080
+
+    /opt/envoy/envoy -c ./envoy.yaml &
+    ./server &
+    sleep 2
+
+    RESPONSE=$(curl -s -X $METHOD \
+        -H "$(echo $HEADERS | /opt/jq/jq -r 'to_entries[] | "\(.key): \(.value)"')" \
+        -d "$BODY" \
+        http://$HOST:$PORT$PATH)
+
     echo $RESPONSE
 }
