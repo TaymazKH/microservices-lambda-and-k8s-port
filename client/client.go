f1e3e25dd8f3b9ae043550bfb3a5e30972e293ed
Taymaz <TaymazKH.U@gmail.com>
2024-09-11 20:23:53 +0330

    updated client to be able to call multiple RPCs

--- client/client.go
+++ client/client.go
@@ -20,9 +20,57 @@ var (
     timeout = flag.Int("timeout", 5, "Timeout in seconds")
 )
 
-// marshalRequest marshals a protobuf request object into a byte array.
-func marshalRequest(helloReq *pb.HelloRequest) ([]byte, error) {
-    binReq, err := proto.Marshal(helloReq)
+const (
+    greeterService = "greeter"
+    sayHelloRPC    = "say-hello"
+    sayByeRPC      = "say-bye"
+)
+
+// sayHello sends a HelloRequest to server and returns a HelloResponse
+func sayHello(helloRequest *pb.HelloRequest) (*pb.HelloResponse, error) {
+    binReq, err := marshalRequest(helloRequest)
+    if err != nil {
+        return nil, err
+    }
+
+    path := fmt.Sprintf("/%s/%s", greeterService, sayHelloRPC)
+    respBody, err := sendRequest(*addr, path, binReq, *timeout)
+    if err != nil {
+        return nil, err
+    }
+
+    msg, err := unmarshalResponse(respBody, path)
+    if err != nil {
+        return nil, err
+    }
+
+    return (*msg).(*pb.HelloResponse), nil
+}
+
+// sayBye sends a ByeRequest to server and returns a ByeResponse
+func sayBye(byeRequest *pb.ByeRequest) (*pb.ByeResponse, error) {
+    binReq, err := marshalRequest(byeRequest)
+    if err != nil {
+        return nil, err
+    }
+
+    path := fmt.Sprintf("/%s/%s", greeterService, sayByeRPC)
+    respBody, err := sendRequest(*addr, path, binReq, *timeout)
+    if err != nil {
+        return nil, err
+    }
+
+    msg, err := unmarshalResponse(respBody, path)
+    if err != nil {
+        return nil, err
+    }
+
+    return (*msg).(*pb.ByeResponse), nil
+}
+
+// marshalRequest marshals a proto message object into a byte array.
+func marshalRequest(msg proto.Message) ([]byte, error) {
+    binReq, err := proto.Marshal(msg)
     if err != nil {
         return nil, fmt.Errorf("failed to marshal request: %v", err)
     }
@@ -30,8 +78,8 @@ func marshalRequest(helloReq *pb.HelloRequest) ([]byte, error) {
 }
 
 // sendRequest sends an HTTP POST request with the given byte array and returns the response body as a byte array.
-func sendRequest(addr string, binReq []byte, timeout int) ([]byte, error) {
-    req, err := http.NewRequestWithContext(context.Background(), "POST", addr, bytes.NewBuffer(binReq))
+func sendRequest(addr, path string, binReq []byte, timeout int) ([]byte, error) {
+    req, err := http.NewRequestWithContext(context.Background(), "POST", addr+path, bytes.NewBuffer(binReq))
     if err != nil {
         return nil, fmt.Errorf("failed to create HTTP request: %v", err)
     }
@@ -56,13 +104,23 @@ func sendRequest(addr string, binReq []byte, timeout int) ([]byte, error) {
     return respBody, nil
 }
 
-// unmarshalResponse unmarshalls a byte array into a protobuf response object.
-func unmarshalResponse(respBody []byte) (*pb.HelloResponse, error) {
-    var helloResp pb.HelloResponse
-    if err := proto.Unmarshal(respBody, &helloResp); err != nil {
-        return nil, fmt.Errorf("failed to unmarshal response: %v", err)
+// unmarshalResponse unmarshalls a byte array into a proto message object.
+func unmarshalResponse(respBody []byte, path string) (*proto.Message, error) {
+    var msg proto.Message
+    switch path {
+    case fmt.Sprintf("/%s/%s", greeterService, sayHelloRPC):
+        msg = &pb.HelloResponse{}
+    case fmt.Sprintf("/%s/%s", greeterService, sayByeRPC):
+        msg = &pb.ByeResponse{}
+    default:
+        return nil, fmt.Errorf("unknown path: %s", path)
     }
-    return &helloResp, nil
+
+    if err := proto.Unmarshal(respBody, msg); err != nil {
+        return nil, fmt.Errorf("failed to unmarshal response: %w", err)
+    }
+
+    return &msg, nil
 }
 
 func main() {
@@ -72,21 +130,23 @@ func main() {
         Name: *name,
     }
 
-    binReq, err := marshalRequest(helloReq)
+    helloResp, err := sayHello(helloReq)
     if err != nil {
-        log.Fatalf("Error marshaling request: %v", err)
-    }
-
-    respBody, err := sendRequest(*addr, binReq, *timeout)
-    if err != nil {
-        log.Fatalf("Error sending request: %v", err)
-    }
-
-    helloResp, err := unmarshalResponse(respBody)
-    if err != nil {
-        log.Fatalf("Error unmarshaling response: %v", err)
+        log.Fatalf("Error calling SayHello RPC: %v", err)
     }
 
     log.Printf("Greeting: %s", helloResp.GetText())
-    fmt.Println(helloResp.GetText())
+
+    byeReq := &pb.ByeRequest{
+        Name: *name,
+    }
+
+    byeResp, err := sayBye(byeReq)
+    if err != nil {
+        log.Fatalf("Error calling SayBye RPC: %v", err)
+    }
+
+    log.Printf("Farewell: %s", byeResp.GetText())
+
+    fmt.Println(helloResp.GetText(), "-", byeResp.GetText())
 }
