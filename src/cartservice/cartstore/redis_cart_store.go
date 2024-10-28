ab985e1142251623b4a2277dafcae05d6f496430
Taymaz <TaymazKH.U@gmail.com>
2024-10-28 09:38:28 +0330

    refactored redis cart store

--- src/cartservice/cartstore/redis_cart_store.go
+++ src/cartservice/cartstore/redis_cart_store.go
@@ -2,25 +2,15 @@ package cartstore
 
 import (
     "context"
-    "encoding/json" // todo: use protojson
     "errors"
     "fmt"
     "log"
 
     "github.com/redis/go-redis/v9"
+    "google.golang.org/protobuf/encoding/protojson"
 
     pb "main/genproto"
 )
-
-type JsonCart struct {
-    UserId string         `json:"userId"`
-    Items  []JsonCartItem `json:"items"`
-}
-
-type JsonCartItem struct {
-    ProductId string `json:"productId"`
-    Quantity  int    `json:"quantity"`
-}
 
 type RedisCartStore struct {
     rdb *redis.Client
@@ -40,25 +30,21 @@ func NewRedisCartStore(redisAddr, redisPassword string) *RedisCartStore {
 func (store *RedisCartStore) AddItemAsync(userId, productId string, quantity int32) error {
     log.Printf("AddItemAsync called with userId=%s, productId=%s, quantity=%d\n", userId, productId, quantity)
 
-    // Get existing cart from Redis
     val, err := store.rdb.Get(store.ctx, userId).Result()
     cart := &pb.Cart{UserId: userId}
 
     if errors.Is(err, redis.Nil) {
-        // Cart not found, create a new one
         cart.Items = []*pb.CartItem{
             {ProductId: productId, Quantity: quantity},
         }
     } else if err != nil {
         return fmt.Errorf("can't access cart storage: %v", err)
     } else {
-        // Unmarshal existing cart and update it
-        err = json.Unmarshal([]byte(val), cart) // todo: define and use new struct
+        err = protojson.Unmarshal([]byte(val), cart)
         if err != nil {
             return fmt.Errorf("error parsing cart: %v", err)
         }
 
-        // Find existing item in the cart
         itemFound := false
         for i, item := range cart.Items {
             if item.ProductId == productId {
@@ -68,14 +54,12 @@ func (store *RedisCartStore) AddItemAsync(userId, productId string, quantity int
             }
         }
 
-        // If item not found, add it
         if !itemFound {
             cart.Items = append(cart.Items, &pb.CartItem{ProductId: productId, Quantity: quantity})
         }
     }
 
-    // Marshal the updated cart and store it in Redis
-    cartData, err := json.Marshal(cart) // todo: define and use new struct
+    cartData, err := protojson.Marshal(cart)
     if err != nil {
         return fmt.Errorf("error serializing cart: %v", err)
     }
@@ -91,18 +75,15 @@ func (store *RedisCartStore) AddItemAsync(userId, productId string, quantity int
 func (store *RedisCartStore) GetCartAsync(userId string) (*pb.Cart, error) {
     log.Printf("GetCartAsync called with userId=%s\n", userId)
 
-    // Get the cart data from Redis
     val, err := store.rdb.Get(store.ctx, userId).Result()
     if errors.Is(err, redis.Nil) {
-        // Cart not found, return an empty cart
         return &pb.Cart{UserId: userId}, nil
     } else if err != nil {
         return nil, fmt.Errorf("can't access cart storage: %v", err)
     }
 
-    // Parse the cart from Redis
     cart := &pb.Cart{}
-    err = json.Unmarshal([]byte(val), cart) // todo: define and use new struct
+    err = protojson.Unmarshal([]byte(val), cart)
     if err != nil {
         return nil, fmt.Errorf("error parsing cart: %v", err)
     }
@@ -113,11 +94,9 @@ func (store *RedisCartStore) GetCartAsync(userId string) (*pb.Cart, error) {
 func (store *RedisCartStore) EmptyCartAsync(userId string) error {
     log.Printf("EmptyCartAsync called with userId=%s\n", userId)
 
-    // Create a new empty cart
-    cart := pb.Cart{UserId: userId}
+    cart := &pb.Cart{UserId: userId}
 
-    // Store the empty cart in Redis
-    cartData, err := json.Marshal(cart) // todo: define and use new struct
+    cartData, err := protojson.Marshal(cart)
     if err != nil {
         return fmt.Errorf("error serializing empty cart: %v", err)
     }
