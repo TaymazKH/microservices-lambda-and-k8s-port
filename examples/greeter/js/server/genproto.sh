71a6da85331a9650668ff49ed7d06f054e454a14
Taymaz <TaymazKH.U@gmail.com>
2024-11-05 15:38:39 +0330

    removed unnecessary js packages

--- examples/greeter/js/server/genproto.sh
+++ examples/greeter/js/server/genproto.sh
@@ -8,5 +8,3 @@ mkdir -p $protoname
 protoc --js_out=import_style=commonjs,binary:./$protoname \
        -I $protodir \
        $protodir/hello.proto
-#       --grpc_out=grpc_js:./$protoname \
-#       --plugin=protoc-gen-grpc=$(which grpc_tools_node_protoc_plugin) \
