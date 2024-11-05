43be7371c136c0b99984ccc69c243ebf07d5f645
Taymaz <TaymazKH.U@gmail.com>
2024-11-05 09:28:36 +0330

    removed the bootstrap and handler scripts from the go client

--- examples/greeter/go/client/handler.sh
+++
@@ -1,9 +0,0 @@
-function handler () {
-    echo "Handler script started." >&2
-
-    RESPONSE=$(./service)
-    echo "Response: $RESPONSE" >&2
-    echo $RESPONSE
-
-    echo "Handler script finished." >&2
-}
