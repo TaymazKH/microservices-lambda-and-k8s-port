da85db21ad5646c94d9f6c2fe9455d7aa773cddd
Taymaz <TaymazKH.U@gmail.com>
2024-12-03 17:33:51 +0330

    added a simple dockerfile for golang grpc service

--- examples/grpc_service/go/server/genproto.sh
+++ examples/grpc_service/go/server/genproto.sh
@@ -8,8 +8,5 @@ mkdir -p $protoname
 protoc --go_opt=Mgreeter.proto="/$protoname" \
        --go_opt=paths=source_relative \
        --go_out=./$protoname \
-       --go-grpc_opt=Mgreeter.proto="/$protoname" \
-       --go-grpc_opt=paths=source_relative \
-       --go-grpc_out=./$protoname \
        -I $protodir \
        $protodir/greeter.proto
